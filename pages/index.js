e5ab9663c955d9585a9efcb598045d15b1cd5e85
jcy0627 <jcy0627@gmail.com>
2023-03-14 13:51:08 +0900

    blog

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>메타빌드 AI 사업센터</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
